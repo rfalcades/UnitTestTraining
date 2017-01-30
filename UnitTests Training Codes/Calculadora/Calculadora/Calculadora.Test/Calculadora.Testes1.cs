d308931c092d06ac199f1faf58e660bb7cf9cebb
Ricardo Falcades <rfalcades@hotmail.com>
2017-01-30 09:22:23 -0200

    Add files via upload

---
+++ UnitTests Training Codes/Calculadora/Calculadora/Calculadora.Test/Calculadora.Testes1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Calculadora.Tests
+{
+    public class Calculadora
+    {
+        [TestFixture]
+        public class CalculatorTest
+        {
+            ICalculator sut;
+            [TestFixtureSetUp]
+            public void TestSetup1()
+            {
+                sut = new Calculator();
+            }
+            [Test]
+            public void ShouldAddTwoNumbers1()
+            {
+
+                int expectedResult = sut.Add(7, 8);
+                Assert.That(expectedResult, Is.EqualTo(15));
+            }
+            [Test]
+            public void ShouldMulTwoNumbers1()
+            {
+
+                int expectedResult = sut.Mul(7, 8);
+                Assert.That(expectedResult, Is.EqualTo(56));
+            }
+
+            [Test]
+            [Ignore]
+            public void ShouldNotMulTwoNumbers()
+            {
+                int expectedResult = sut.Mul(7, 8);
+                Assert.That(expectedResult, Is.EqualTo(15));
+            }
+
+
+            [TestFixtureTearDown]
+            public void TestTearDown1()
+            {
+                sut = null;
+            }
+
+        }
+
+    }
+}
